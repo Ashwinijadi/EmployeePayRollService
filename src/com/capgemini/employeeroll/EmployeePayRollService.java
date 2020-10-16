ea77817636f76aa3548a4c54e0689a7c5ac4cd43
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-16 20:32:45 +0530

    added: Welcome to EmployeePayRollService programming

---
+++ src/com/capgemini/employeeroll/EmployeePayRollService.java
@@ -0,0 +1,10 @@
+package com.capgemini.employeeroll;
+
+public class EmployeePayRollService {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to EmployeeRoll service programming");
+	}
+
+}
