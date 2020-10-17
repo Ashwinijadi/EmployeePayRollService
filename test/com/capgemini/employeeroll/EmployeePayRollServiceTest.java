c791291be4388b1e5f2dcdd41d543df3a23d50c8
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-17 10:35:48 +0530

    added: Functionality of Showing number of entries and printing the employee payrolls

--- test/com/capgemini/employeeroll/EmployeePayRollServiceTest.java
+++ test/com/capgemini/employeeroll/EmployeePayRollServiceTest.java
@@ -20,6 +20,28 @@ public class EmployeePayRollServiceTest {
 		Assert.assertEquals(3, entries);
 	}
 
-	
-	
+	@Test
+	public void writePayrollOnFile() {
+		EmployeePayRollData[] Employees = { new EmployeePayRollData(1, "ashu", 100000.0),
+				new EmployeePayRollData(2, "shiny", 200000.0), new EmployeePayRollData(3, "joy", 300000.0) };
+		EmployeePayRollService employeePayRollService1;
+		employeePayRollService1 =new EmployeePayRollService(Arrays.asList(Employees));
+		employeePayRollService1.writeEmployeePayrollData(FILE_IO);
+		employeePayRollService1.printData(FILE_IO);
+		long entries = employeePayRollService1.countEntries(FILE_IO);
+		Assert.assertEquals(3, entries);
+	}
+
+	@Test
+	public void countEntries() {
+		EmployeePayRollData[] Employee = { new EmployeePayRollData(1, "ash", 100000.0),
+				new EmployeePayRollData(2, "sweet", 200000.0), new EmployeePayRollData(3, "smile", 300000.0) };
+		EmployeePayRollService employeePayRollService;
+		 employeePayRollService= new EmployeePayRollService(Arrays.asList(Employee));
+		employeePayRollService.writeEmployeePayrollData(FILE_IO);
+		employeePayRollService.printData(FILE_IO);
+		long entries = employeePayRollService.countEntries(FILE_IO);
+		System.out.println("number of entries : "+entries);
+		Assert.assertEquals(3, entries);
+	}
 }
